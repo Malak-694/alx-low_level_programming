1c81ec897be854171315a5d1bc896a57c9a3b2f7
Malak-694 <malakeltabakh2020@gmail.com>
2023-02-16 01:24:45 -0800

    o

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by anwline."
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by anwline."
  * Rturn Always 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
